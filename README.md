7032862037388bd8cdd66d620ccce02767e744e4
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2017-03-05 23:14:48 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Play Super Mario (web game) with your voice
+![screenshot](screenshot.png)
+
+By playing with the volume of the current microphone registered audio/noise you can command Super Mario.
+
+Funny video of my housemate playing (click for the video):
+[![youtube video](video_youtube.gif)](https://youtu.be/7neh0ieFx0E)
+
+# Install
+You'll need:
+* [PyQt4](https://pypi.python.org/pypi/PyQt4) `sudo apt-get install python-qt4`
+* [pyqtgraph](http://www.pyqtgraph.org/) `sudo pip install pyqtgraph`
+* [numpy](http://www.numpy.org/) `sudo apt-get install python-numpy`
+* [pykeyboard](https://github.com/SavinaRoja/PyUserInput/tree/master/pykeyboard) `sudo pip install PyUserInput`
+* [pyaudio](https://people.csail.mit.edu/hubert/pyaudio/) `sudo pip install pyaudio`. **Note**: You may get rid of this dependence modifying the code using the hacky approach found in [this gist](https://gist.github.com/awesomebytes/a382d94c0b312d9b507051b99a433a31). Do it at your own risk.
+
+# Run
+
+    python play_mario.py
+
+And now when the sound registered by your microphone is louder than 12.5% **D** key will be pressed (so Mario will start moving right) when the noise becomes lower the key will be released. When the audio is louder than 25.0% **W** will be pressed (Mario will jump as high as relative to the maximum volume found at that moment). So if you make a louder noise it will jump higher!
+
+You can adjust these parameters as you wish in the code.
+
+# Thanks to
+Based on  [qt audio monitor](https://github.com/swharden/Python-GUI-examples).
+
+PROJECT PAGE: http://www.swharden.com/wp/2016-07-31-real-time-audio-monitor-with-pyqt/
+
+![demo](demo.gif)
---
+++ play_mario.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python
+from PyQt4 import QtGui, QtCore
+
+import sys
+import ui_main
+import numpy as np
+import pyqtgraph
+import SWHear
+# sudo pip install PyUserInput
+from pykeyboard import PyKeyboard
+import time
+
+# Use audio to play super mario: http://supermarioemulator.com/mario.php
+# A minimum volume of noise of 12.5% will make mario walk
+# Any sound long enough with noise of >25.0% will make mario jump
+# The louder the sound the higher the jump
+
+# Author: Sammy Pfeiffer <sammypfeiffer at gmail.com>
+
+
+class ExampleApp(QtGui.QMainWindow, ui_main.Ui_MainWindow):
+    def __init__(self, parent=None):
+        pyqtgraph.setConfigOption('background', 'w')  # before loading widget
+        super(ExampleApp, self).__init__(parent)
+        self.setupUi(self)
+        self.grFFT.plotItem.showGrid(True, True, 0.7)
+        self.grPCM.plotItem.showGrid(True, True, 0.7)
+        self.maxFFT = 0
+        self.maxPCM = 32767  # Hardcoding it for S16_LE
+        self.ear = SWHear.SWHear(rate=44100, updatesPerSecond=30)
+        self.ear.stream_start()
+        self.k = PyKeyboard()
+        self.pressed_d = False
+        self.pressed_w = False
+        self.max_jump_time = 0.5
+        self.jump_time = 0.0
+
+    def update(self):
+        if self.ear.data is not None and self.ear.fft is not None:
+            pcmMax = np.max(np.abs(self.ear.data))
+            if pcmMax > self.maxPCM:
+                self.maxPCM = pcmMax
+                self.grPCM.plotItem.setRange(yRange=[-pcmMax, pcmMax])
+            if np.max(self.ear.fft) > self.maxFFT:
+                self.maxFFT = np.max(np.abs(self.ear.fft))
+                # self.grFFT.plotItem.setRange(yRange=[0,self.maxFFT])
+                # self.grFFT.plotItem.setRange(yRange=[0, 1])
+                self.grFFT.plotItem.setRange(yRange=[0, 0.5])
+
+            self.pbLevel.setValue(1000 * pcmMax / self.maxPCM)
+
+            pcmMax_pc = (float(pcmMax) / float(self.maxPCM)) * 100.0
+            pcmAvg = np.average(np.abs(self.ear.data))
+            pcmAvg_pc = (float(pcmAvg) / float(self.maxPCM)) * 100.0
+            print "pcmMax: " + str(round(pcmMax_pc, 3)) + " % (" + str(pcmMax) + ")"
+            print "pcmAvg: " + str(round(pcmAvg_pc, 3)) + " %",
+            if self.pressed_d:
+                print 'D ',
+            else:
+                print '  ',
+            if self.pressed_w:
+                print 'W'
+            else:
+                print
+            print "Jump time: " + str(self.jump_time)
+            print
+            print
+            if pcmAvg_pc > 12.5:
+                if not self.pressed_d:
+                    print "Down D"
+                    self.k.press_key('d')
+                    self.pressed_d = True
+            else:
+                if self.pressed_d:
+                    print "Up D"
+                    self.k.release_key('d')
+                    self.pressed_d = False
+            if pcmAvg_pc > 25.0:
+                if not self.pressed_w:
+                    print "Down W"
+                    self.k.press_key('w')
+                    self.pressed_w = True
+                    curr_time = time.time()
+                    self.jump_time = self.max_jump_time * (float(pcmMax_pc) / 100.0)
+                    self.release_jump_time = curr_time + self.jump_time
+            else:
+                if self.pressed_w:
+                    if time.time() >= self.release_jump_time:
+                        print "Up W"
+                        self.k.release_key('w')
+                        self.pressed_w = False
+            pen = pyqtgraph.mkPen(color='b')
+            self.grPCM.plot(self.ear.datax, self.ear.data, pen=pen, clear=True)
+            pen = pyqtgraph.mkPen(color='r')
+            self.grFFT.plot(self.ear.fftx, self.ear.fft /
+                            self.maxFFT, pen=pen, clear=True)
+        QtCore.QTimer.singleShot(1, self.update)  # QUICKLY repeat
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    form = ExampleApp()
+    form.show()
+    form.update()  # start with something
+    app.exec_()
+    print("DONE")
